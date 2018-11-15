127a9fc9565e35f571055be577b92eea4760d356
annamariameschede <44995589+annamariameschede@users.noreply.github.com>
2018-11-15 20:06:05 +0800

    passenger request ride

--- Passenger.java
+++ Passenger.java
@@ -12,42 +12,7 @@ public class Passenger
         this.con = con;
     }
 
-	public void requestRide() 
-	{
-	    try{
-            int pid, num_passengers, model_year;
-            String model;
-
-            do { //ask for input until pid is inserted and is in correct format
-                System.out.println("Please enter your ID.");
-                while (!sc.hasNextInt()) {
-                    System.out.println("You must enter a valid integer ID.");
-                    sc.next();
-                }
-                pid = sc.nextInt();
-            } while (pid != null);
-
-            do { //ask for input until num_passengers is inserted and is in correct format
-                System.out.println("Please enter the number of passengers.");
-                while (!sc.hasNextInt()) {
-                    System.out.println("You must enter a valid number of passengers.");
-                    sc.next();
-                }
-                num_passengers = sc.nextInt();
-            } while (num_passengers != null);
-
-            System.out.println("Please enter the earliest model year (optional). Press enter to skip.");
-            model_year = sc.nextInt();
-            System.out.println("Please enter the vehicle model (optional). Press enter to skip.");
-            model = sc.nextLine();
-
-            //search for vehicles available (not in unfinished trips)
-            //place request for matching trip
-        }catch (SQLException e) {
-            System.out.println(e);
-        }
-
-	}
+    static int requestID = 0;				//request ID of last created request (negative integers)
 	
 	
 	
@@ -70,6 +35,161 @@ public class Passenger
         }
 
 	}
+	
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+	public void requestRide() 
+	{
+		try
+		{
+			int passengerID, seats;				//variables for data entered by passenger
+			String model_year, model;
+			
+			//read in passengers request
+			System.out.println("Please enter your ID.");				//read in passengerID
+			passengerID = sc.nextInt();
+			System.out.println("Please enter the number of passengers.");				//read in number of seats
+			seats = sc.nextInt();
+			System.out.println("Please enter the earliest model year. (Press ENTER to skip)"); 				//read in model_year, in string format instead of integer format, no model_year possible  
+			Scanner charScanner = new Scanner(System.in).useDelimiter("\\n");
+			model_year = "";
+			model_year += charScanner.nextLine();
+			if(model_year.equals("")) model_year = null;
+			System.out.println("Please enter the model.(Press ENTER to skip)");				//read in model, no model possible
+			model = "";
+			model += charScanner.nextLine();	
+			if(model.equals("")) model = null;
+			
+			
+			
+			//SQL-Query to find out whether there are cars available matching the passengers request that are not in unfinished trips
+			String sqlstart = "SELECT D.driverID FROM Driver D WHERE D.seats >= ?";			
+			String model_yearsql = " AND D.model_year >= ?";
+			String modelsql = " AND D.model LIKE ?";
+			String sqlend = " AND D.driverID NOT IN (SELECT D2.driverID FROM Driver D2, Trip T WHERE T.driverID = D2.driverID AND T.end IS NULL)";
+
+			PreparedStatement stmt;
+			ResultSet rs;
+			
+			if(model_year == null)
+			{
+				if(model == null)				//no model_year preference, no model preference
+				{
+					stmt = con.prepareStatement(sqlstart + sqlend);
+					stmt.setInt(1, seats);
+				}
+				else				//no model_year preference but given model preference
+				{
+					stmt = con.prepareStatement(sqlstart + modelsql + sqlend); 
+					stmt.setInt(1, seats);
+					String modelstring = "%" + model + "%";
+					stmt.setString(2, modelstring);
+				}
+			}
+			else
+			{
+				if(model == null)				//given model_year preference but no model preference
+				{
+					stmt = con.prepareStatement(sqlstart + model_yearsql + sqlend); 
+					stmt.setInt(1, seats);
+					int model_yearint = Integer.parseInt(model_year);
+					stmt.setInt(2, model_yearint);
+				}
+				else				//given model_year preference and given model preference
+				{
+					stmt = con.prepareStatement(sqlstart + model_yearsql + modelsql + sqlend);
+					stmt.setInt(1, seats);
+					int model_yearint = Integer.parseInt(model_year);
+					stmt.setInt(2, model_yearint);
+					String modelstring = "%" + model + "%";
+					stmt.setString(3, modelstring);
+				}
+			}
+			
+			
+			rs = stmt.executeQuery();
+			
+			if(!rs.isBeforeFirst())				//empty resultset, no car matching the the passengers request available at the moment
+			{
+				System.out.println("Sorry, no cars available matching your request");
+			}
+			else				//cars available matching the passengers request, create a new request and display number of available drivers (including those in unfinished trips)
+			{
+				//calculate the number of available drivers
+				sqlstart = "SELECT COUNT(*) FROM Driver D WHERE seats >= ?";
+			
+				if(model_year == null)
+				{
+					if(model == null)				//no model_year preference, no model preference
+					{
+						stmt = con.prepareStatement(sqlstart);
+						stmt.setInt(1, seats);
+					}
+					else				//no model_year preference but given model preference
+					{
+						stmt = con.prepareStatement(sqlstart + modelsql); 
+						stmt.setInt(1, seats);
+						String modelstring = "%" + model + "%";
+						stmt.setString(2, modelstring);
+					}
+				}
+				else
+				{
+					if(model == null)				//given model_year preference but no model preference
+					{
+						stmt = con.prepareStatement(sqlstart + model_yearsql); 
+						stmt.setInt(1, seats);
+						int model_yearint = Integer.parseInt(model_year);
+						stmt.setInt(2, model_yearint);
+					}
+					else				//given model_year preference and given model preference
+					{
+						stmt = con.prepareStatement(sqlstart + model_yearsql + modelsql);
+						stmt.setInt(1, seats);
+						int model_yearint = Integer.parseInt(model_year);
+						stmt.setInt(2, model_yearint);
+						String modelstring = "%" + model + "%";
+						stmt.setString(3, modelstring);
+					}
+				}
+				rs = stmt.executeQuery();
+				rs.next();
+				System.out.println("Your request is placed. " + rs.getInt(1) + " drivers are able to take your request.");
+				
+				
+				//create new request
+				requestID -= 1;				//decrement requestID
+				String sql = "INSERT INTO Request VALUES (?, 0, ?, ?, ?, ?)";				//for untaken requests taken field has value 0
+				stmt = con.prepareStatement(sql);
+				stmt.setInt(1, requestID);
+				stmt.setInt(4, seats);
+				stmt.setInt(5, passengerID);
+				if(model_year == null)				//no model_year preference
+				{
+					stmt.setNull(2, Types.INTEGER);				//set model_year field NULL
+				}
+				else				//with model_year preference 
+				{
+					stmt.setInt(2, Integer.parseInt(model_year));
+				}
+				if(model == null)				//no model preference
+				{
+					stmt.setString(3, null);				//set model field NULL
+				}
+				else				//with model preference
+				{
+					stmt.setString(3, model);
+				}
+				stmt.executeUpdate();
+			}
+
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e);
+		}	
+	}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	
 	
 	
@@ -107,9 +227,6 @@ public class Passenger
         } catch (SQLException e) {
             System.out.println(e);
         }
-
-
-
     }
 	
 	
